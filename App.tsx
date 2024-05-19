85609399c9dd502d2d753cc9b5104cf8a68b5903
ahmedgit2 <ahmedmoh6000@gmail.com>
2024-05-20 02:07:11 +0300

    test

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {checkUpdate} from './src/utils/dev';
 import {TestScreen} from './src/screens/TestScreen/test-screen';
 
 function App(): React.JSX.Element {
-  if (!__DEV__) checkUpdate();
+  checkUpdate();
 
   return <TestScreen />;
 }
--- src/screens/TestScreen/style.ts
+++ src/screens/TestScreen/style.ts
@@ -3,12 +3,12 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   Container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#f5f5f5',
   },
   Text: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: '#fff',
+    color: '#000',
     textAlign: 'center',
     paddingTop: 100,
   },
